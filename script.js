555a2da980d96fe6d50f648c08cf05f13878b412
Archer Key <archerbkey@gmail.com>
2024-12-03 16:10:02 +0800

    removed the music website content

--- script.js
+++ script.js
@@ -1,11 +1,93 @@
+function startNavBarAnimation() {
+  let id = null;
+  const element = document.getElementById("navTitle");
+  let text = element.innerHTML;
+  let textLength = text.length;
+  let i = 1;
+  clearInterval(id);
+  id = setInterval(typeText, 50);
+  function typeText() {
+    element.innerHTML = text.substr(0, i);
+    if (i == textLength){ 
+      clearInterval(id); 
+      underscoreAnimation(); }
+    else { 
+      i++; 
+    }
+  }
+}
+
+function typeOutText(text) {
+  let id = null;
+  const element = document.getElementById("navTitle");
+  let textLength = text.length;
+  let i = 1;
+  clearInterval(id);
+  id = setInterval(typeText, 50);
+  function typeText() {
+    element.innerHTML = text.substr(0, i);
+    if (i == textLength){ 
+      clearInterval(id); 
+      underscoreAnimation(); }
+    else { 
+      i++; 
+    }
+  }
+}
+function underscoreAnimation() {
+  let id = null;
+  const element = document.getElementById("navTitle");
+  let text = element.innerHTML; 
+  let i = 0;
+  let underscore = 0;
+  clearInterval(id);
+  id = setInterval(animateUnderscore, 800);
+  function animateUnderscore() {
+    if (i == 6){ 
+      clearInterval(id); 
+      deleteText();
+    }
+    else { 
+      i++;
+      if (underscore) {
+        element.innerHTML = text;
+        underscore = 0;
+      }
+      else {
+        element.innerHTML = text + "_";
+        underscore = 1;
+      }
+    }
+  }
+}
+
+function deleteText() {
+  let id = null;
+  const element = document.getElementById("navTitle");
+  let text = element.innerHTML;
+  let textLength = text.length;
+  let i = textLength;
+  clearInterval(id);
+  id = setInterval(deleteText, 50);
+  function deleteText() {
+    element.innerHTML = text.substr(0, i);
+    if (i == 1){ 
+      clearInterval(id); 
+      typeOutText(text);
+    }
+    else { 
+      i--; 
+    }
+  }
+}
+
 function homeDVDAnimation() {
-  console.log("Animation played");
   let id = null;
   const container = document.getElementById("homeContent");
   const element = document.getElementById("titleCard");
   let x = 0; //container.offsetWidth * 0.5 - element.offsetWidth * 0.5;
   let y = 0; //container.offsetHeight * 0.4;
-  let xSpeed = 1.5;
+  let xSpeed = 1.25;
   let ySpeed = 1;
   clearInterval(id);
   id = setInterval(frame, 5);
@@ -32,3 +114,4 @@ function homeDVDAnimation() {
     element.style.top = y + "px";
   }
 }
+
